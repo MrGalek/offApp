8e7f5b178c8e1bb3fd3c550e78f817e3cea60247
Damian Gałczyński <mr_galek@hotmail.com>
2017-09-01 16:35:57 +0200

    First commit

---
+++ offApp/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace offApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        SystemSwitch systemSwitch = new SystemSwitch();
+        public MainWindow()
+        {
+            InitializeComponent();
+            systemSwitch = new SystemSwitch();
+            editLabel("Brak zaplanowanego zamknięcia systemu Windows");
+            date.DisplayDateStart = DateTime.Today;
+        }
+
+        private void editLabel(String newText)
+        {
+            statusLabel.Content = newText;
+        }
+
+        private void addShutdown_Click(object sender, RoutedEventArgs e)
+        {
+            backBorder();
+            bool validForm = true;
+            if (hourTextBox.Text == "" || Int32.Parse(hourTextBox.Text) > 24)
+            {
+                hourTextBox.BorderBrush = Brushes.Red;
+                validForm = false;
+            }
+            if (minutesTextBox.Text == "" || Int32.Parse(minutesTextBox.Text)>60)
+            {
+                minutesTextBox.BorderBrush = Brushes.Red;
+                validForm = false;
+            }
+            if (date.Text == "")
+            {
+                date.BorderBrush = Brushes.Red;
+                validForm = false;
+            }
+
+            if (validForm)
+            {
+                systemSwitch.addShutdown(date.SelectedDate.Value, hourTextBox.Text, minutesTextBox.Text);
+                editLabel("Zaplanowane zamknięcie na: " + systemSwitch.getShutdownDate());
+            }
+        }
+        private void backBorder()
+        {
+            hourTextBox.BorderBrush = Brushes.Black;
+            minutesTextBox.BorderBrush = Brushes.Black;
+            date.BorderBrush = Brushes.Black;
+        }
+    }
+}
---
+++ offApp/SystemSwitch.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace offApp
+{
+    class SystemSwitch
+    {
+        private DateTime day;
+        private String hour;
+        private String minutes;
+
+       public SystemSwitch()
+        {
+
+        }
+    
+        public void addShutdown(DateTime day,String hour,String minutes)
+        {
+            this.day = day;
+            this.hour = hour;
+            this.minutes = minutes;
+            this.day = this.day.AddHours(Double.Parse(hour));
+            this.day = this.day.AddMinutes(Double.Parse(minutes));
+
+            getCommandShutdown(calculateSecunds());
+        }
+
+        public String getShutdownDate()
+        {
+            return day.ToString();
+        }
+
+        private void getCommandShutdown(uint secunds)
+        {
+            String args = "/s /t " + secunds.ToString();
+            Process.Start("shutdown",args);
+        }
+
+        private uint calculateSecunds()
+        {
+            DateTime now = DateTime.Now;
+            TimeSpan discr = day-now;
+            double secunds = discr.TotalSeconds;
+            return Convert.ToUInt32(secunds);
+        }
+    }
+}
